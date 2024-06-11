20775d3a7e2b373db6480c2ee9e7d5e19cf2ca4a
Colin Chauche <colin.chauche@gmail.com>
2024-06-11 14:33:37 -0600

    complete section 28

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,18 @@ class User < ApplicationRecord
             uniqueness: {
               case_sensitive: false
             }
+
+  validates :username,
+            presence: true,
+            uniqueness: {
+              case_sensitive: false
+            },
+            format: {
+              with: /\A(\w|\d)+\z/,
+              message:
+                "must consist of only numbers and digits with no white spaces."
+            },
+            length: {
+              minimum: 3
+            }
 end
