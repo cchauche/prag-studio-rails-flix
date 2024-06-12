03ec4dce495a3cba2f16029f17527500ba16134f
Colin Chauche <colin.chauche@gmail.com>
2024-06-12 15:13:19 -0600

    complete section 33p1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   add_flash_types(:danger)
+
+  private
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  helper_method :current_user
+
+  def require_signin
+    redirect_to signin_url, alert: "Please sign in first!" unless current_user
+  end
 end
