458005709456f7e726c58a7f21d07596e932c77b
Colin Chauche <colin.chauche@gmail.com>
2024-06-17 15:29:49 -0600

    complete section 34

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,10 +15,22 @@ class ApplicationController < ActionController::Base
 
   helper_method :current_user?
 
+  def current_user_admin?
+    current_user && current_user.admin?
+  end
+
+  helper_method :current_user_admin?
+
   def require_signin
     unless current_user
       session[:intended_url] = request.url
-      redirect_to session, alert: "Please sign in first!"
+      redirect_to root_url, alert: "Please sign in first!"
+    end
+  end
+
+  def require_admin
+    unless current_user_admin?
+      redirect_to root_url, alert: "Unauthorized access!"
     end
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :require_signin, except: %i[new create]
-  before_action :require_correct_user, only: %i[edit update destroy]
+  before_action :require_correct_user, only: %i[edit update]
+  before_action :require_admin, only: [:destroy]
 
   def index
     @users = User.all
@@ -37,10 +38,10 @@ class UsersController < ApplicationController
   end
 
   def destroy
+    @user = User.find(params[:id])
     @user.destroy
-    session[:user_id] = nil
 
-    redirect_to movies_url,
+    redirect_to root_url,
                 status: :see_other,
                 alert: "Account succesfully deleted!"
   end
