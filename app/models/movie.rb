248bc2c4a6bbaeb7d230371bce1f41d3899b53c5
Colin Chauche <colin.chauche@gmail.com>
2024-06-24 15:07:20 -0600

    complete section 38p1

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,22 +3,24 @@ class Movie < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :fans, through: :favorites, source: :user
   has_many :critics, through: :reviews, source: :user
+  has_many :characterizations, dependent: :destroy
+  has_many :genres, through: :characterizations
 
   RATINGS = %w[G PG PG-13 R NC-17]
 
   validates :title, :released_on, :duration, presence: true
 
-  validates :description, length: { minimum: 25 }
+  validates :description, length: {minimum: 25}
 
-  validates :total_gross, numericality: { greater_than_or_equal_to: 0 }
+  validates :total_gross, numericality: {greater_than_or_equal_to: 0}
 
   validates :image_file_name,
-            format: {
-              with: /\w+\.(jpg|png)\z/i,
-              message: "must be a JPG or PNG image"
-            }
+    format: {
+      with: /\w+\.(jpg|png)\z/i,
+      message: "must be a JPG or PNG image"
+    }
 
-  validates :rating, inclusion: { in: RATINGS }
+  validates :rating, inclusion: {in: RATINGS}
 
   def self.released
     where("released_on < ?", Time.now).order("released_on desc")
